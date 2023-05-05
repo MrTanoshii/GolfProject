6db08827a54c4e8881a3ddc320313f13fed3712b
Jacob <122863612+jacobgreg27@users.noreply.github.com>
2023-05-05 15:35:41 +0400

    Update urls.py

--- django_sms/django_sms/urls.py
+++ django_sms/django_sms/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from homepage.views import HomePageView
 from django.urls import path,include
 
 
@@ -26,7 +25,7 @@ urlpatterns = [
     path('smsApp/', include('smsApp.urls')),
     path('homepage/', include('homepage.urls')),
     path('scanner/', include('scanner.urls')),
-    path('',HomePageView.as_view(), name="home-page"),
+    
     
 ]
 hander404 = 'smsApp.views.error_404_view'

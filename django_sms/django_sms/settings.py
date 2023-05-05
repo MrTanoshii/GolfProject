fc9df9f9371042c7f6b8fdf0a762ac582cbf106d
Jacob <122863612+jacobgreg27@users.noreply.github.com>
2023-05-05 15:04:05 +0400

    Update settings.py

--- django_sms/django_sms/settings.py
+++ django_sms/django_sms/settings.py
@@ -90,13 +90,19 @@ WSGI_APPLICATION = 'django_sms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
